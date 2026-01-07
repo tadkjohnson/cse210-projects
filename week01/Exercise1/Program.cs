9744e0dca5e171fb3db378dc36a851764d2bf802
tadkjohnson <tadkjohnson@gmail.com>
2026-01-06 20:17:02 -0600

    finished first exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("Hello, What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Are you really {last}, {first} {last}? ");
+        Console.WriteLine();
     }
 }
