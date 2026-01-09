09ae233c84be4f92dc8c6edc3677b4c8165914ec
tadkjohnson <tadkjohnson@gmail.com>
2026-01-08 20:32:05 -0600

    starting the list assignment 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,16 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        List<string> words = new List<string>();
+
+
+
+
+
     }
 }
