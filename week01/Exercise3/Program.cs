1b484c22b1a42ecf38186e75e1f914b417ed5a21
tadkjohnson <tadkjohnson@gmail.com>
2026-01-08 20:10:16 -0600

    problem 3 solved?

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,71 +1,48 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Security.Cryptography;
+using System.Collections.Concurrent;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int x = RandomNumberGenerator.Next(1, 101);   // to get 100 numbers instead of 99
+        bool playAgain = true;
 
 
         Console.WriteLine("Hello World! This is the Exercise 3 Project.");
         Console.WriteLine("We are going to play a guess the number game");
         Console.WriteLine("I will let you know if you are to high, low, or correct.");
 
-
+        while (playAgain)
         {
-            Console.Write("What is your guess? ");
-            string valueFromUser = Console.ReadLine();
-            int guess = int.Parse(valueFromUser);
 
-            if (guess > x)
+            int x = randomGenerator.Next(1, 101);   // to get 100 numbers instead of 99Console.Write("What is your guess? ");
+            bool isCorrect = false;
+
+            while (!isCorrect)
             {
-                Console.Write("Your guess it is to high");
+                Console.Write("what is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess > x)
+                {
+                    Console.WriteLine("Your guess it is to high");
+                }
+                else if (guess < x)
+                {
+                    Console.WriteLine("Your guess it is to low");
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                    isCorrect = true;
+                }
             }
-            else if (guess < x)
-            {
-                Console.Write("Your guess it is to low");
-            }
-
-
-
-            Console.WriteLine($"Random number; {x}");
-
-
-
-
-
-
-
-            Console.WriteLine("Want to play a game Y/N? ");
-            string valueFromUser2 = Console.ReadLine();
-
-            //        if valueFromUser2 = Y
-            //        if valueFromUser2 = N
-            //            else;
-            {
-                //          Console.WriteLane("Thanks for playing");
-            }
-
-
-
+            Console.Write("Do you want to play again? (Y/N) ");
+            string answer = Console.ReadLine();
+            if (answer == N)
+                playAgain = false;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+}
