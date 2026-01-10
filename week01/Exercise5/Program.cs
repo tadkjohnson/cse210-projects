0a84de9a80c6a5a8d314cc167070d88e44d3d8ed
tadkjohnson <tadkjohnson@gmail.com>
2026-01-10 12:59:02 -0600

    working on 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,96 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        // main messages
+        
+        Static void Main(string[] args)
+    {
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int getSquared = getSquared(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+
     }
+
+    // welcome message function
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the machine err Program!");
+    }
+
+    // user name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your user name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+
+    // user number 
+    static int favoriteNumber()
+    {
+        float favoriteNumber = Console.ReadLine();
+        Return favoriteNumber;
+    }
+
+    // number squared away
+    static int getSquared(int favoriteNumber)
+    {
+        return getSquared * getSquared;
+
+    }
+    {
+        Console.WriteLine($"You are {name}, your favorite number is {favoriteNumber}, and that number squared is (getSquared)");
+    }
+
+
+
 }
+
+
+
+
+
+
+// Console.WriteLine("Welcome to the program!");
+// Console.Writeline("Please enter your name: ");
+// string name = Console.ReadLine();
+
+// Console.Writeline("Please Enter your Favorite number: ");
+// int numberFavorite = Console.ReadLine();
+
+
+// Console.Writeline("");
+// Console.Writeline($"{name} the square of your number is {squaredNumber}");
+// Console.Writeline("");
+
+
+
+
+
+
+
+
+//     }
+
+
+
+
+
+
+
+
+
+// }
