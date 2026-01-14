e73f309ece02a0ae91a34a40bff8575a2cc1017a
tadkjohnson <tadkjohnson@gmail.com>
2026-01-13 19:39:01 -0600

    Resume program complete

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,13 +7,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Fun Pimps";
         job1._startYear = 2020;
         job1._endYear = 2025;
 
-        job1.Display();
+        // job1.Display();
 
         // Console.WriteLine(job1._jobTitle);
         // Console.WriteLine(job1._company);
@@ -26,12 +28,25 @@ class Program
         job2._startYear = 1995;
         job2._endYear = 2020;
 
-        job2.Display();
+        // job2.Display();
 
         // Console.WriteLine(job2._jobTitle);
         // Console.WriteLine(job2._company);
         // Console.WriteLine(job2._startYear);
         // Console.WriteLine(job2._endYear);
+
+        // create resume and persons name
+
+        Resume myResume = new Resume();
+        myResume._name = "James Bond";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Console.WriteLine(myResume._jobs[0]._jobTitle);
+
+        myResume.Display();
+
 
     }
 
