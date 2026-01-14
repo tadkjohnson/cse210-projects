8ee8d6c9d902b10c54f2569fb2bf09abcc7f54b0
tadkjohnson <tadkjohnson@gmail.com>
2026-01-13 19:23:36 -0600

    done to step 5

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
+
 class Program
+
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Fun Pimps";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
+
+        job1.Display();
+
+        // Console.WriteLine(job1._jobTitle);
+        // Console.WriteLine(job1._company);
+        // Console.WriteLine(job1._startYear);
+        // Console.WriteLine(job1._endYear);
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Nintendo";
+        job2._startYear = 1995;
+        job2._endYear = 2020;
+
+        job2.Display();
+
+        // Console.WriteLine(job2._jobTitle);
+        // Console.WriteLine(job2._company);
+        // Console.WriteLine(job2._startYear);
+        // Console.WriteLine(job2._endYear);
+
     }
+
 }
