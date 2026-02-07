1dd5de0fe4ad54212a19640c536a04e23e8244c0
tadkjohnson <tadkjohnson@gmail.com>
2026-02-07 16:24:13 -0600

    mostly works.  out of time.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment a1 = new Assignment("John Doe", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Jane Dough". "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        MathAssignment a3 = new MathAssignment("Lux Luthor". "Division", "Get Superman");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetHomeworkList());
+
+
+
     }
 }
